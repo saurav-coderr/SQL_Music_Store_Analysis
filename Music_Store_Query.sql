edc8494a53258a237fb459f7774858cea12793bf
saurav-coderr <sauravsomavanshiss111@gmail.com>
2024-08-28 16:09:49 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -12,8 +12,8 @@ LIMIT 1
 
 SELECT COUNT(*) AS c, billing_country 
 FROM invoice
-GROUP BY billing_country
-ORDER BY c DESC
+GROUP BY billing_country   # Group by use for count at seaprte for every country otherwise count its is the all combine
+ORDER BY c DESC        
 
 
 /* Q3: What are top 3 values of total invoice? */
@@ -21,12 +21,15 @@ ORDER BY c DESC
 SELECT total 
 FROM invoice
 ORDER BY total DESC
+limit 3
+
 
 
 /* Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. 
-Write a query that returns one city that has the highest sum of invoice totals. 
+Write a query that returns one city that has the highest sum of invoice totals. ( sum of total )
 Return both the city name & sum of all invoice totals */
-
+{ Which city is sum of total invoices is highest }
+	
 SELECT billing_city,SUM(total) AS InvoiceTotal
 FROM invoice
 GROUP BY billing_city
@@ -39,8 +42,8 @@ Write a query that returns the person who has spent the most money.*/
 
 SELECT customer.customer_id, first_name, last_name, SUM(total) AS total_spending
 FROM customer
-JOIN invoice ON customer.customer_id = invoice.customer_id
-GROUP BY customer.customer_id
+JOIN invoice ON customer.customer_id = invoice.customer_id    # join use for add two address like customer and invoices 
+GROUP BY customer.customer_id                                 # want to add then need two comman element like customer_id in both addesses.
 ORDER BY total_spending DESC
 LIMIT 1;
 
@@ -50,7 +53,7 @@ LIMIT 1;
 /* Question Set 2 - Moderate */
 
 /* Q1: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
-Return your list ordered alphabetically by email starting with A. */
+Return your list ordered alphabetically by email starting with A. (A means ascending */
 
 /*Method 1 */
 
@@ -61,7 +64,7 @@ JOIN invoiceline ON invoice.invoice_id = invoiceline.invoice_id
 WHERE track_id IN(
 	SELECT track_id FROM track
 	JOIN genre ON track.genre_id = genre.genre_id
-	WHERE genre.name LIKE 'Rock'
+	WHERE genre.name LIKE 'Rock'     # rock is use for category of track of genre
 )
 ORDER BY email;
 
@@ -73,23 +76,23 @@ FROM customer
 JOIN invoice ON invoice.customer_id = customer.customer_id
 JOIN invoiceline ON invoiceline.invoice_id = invoice.invoice_id
 JOIN track ON track.track_id = invoiceline.track_id
-JOIN genre ON genre.genre_id = track.genre_id
-WHERE genre.name LIKE 'Rock'
-ORDER BY email;
+JOIN genre ON genre.genre_id = track.genre_id         
+WHERE genre.name LIKE 'Rock'                      
+ORDER BY email;     # by deafult increasing order
 
 
 /* Q2: Let's invite the artists who have written the most rock music in our dataset. 
 Write a query that returns the Artist name and total track count of the top 10 rock bands. */
 
 SELECT artist.artist_id, artist.name,COUNT(artist.artist_id) AS number_of_songs
-FROM track
+FROM track 
 JOIN album ON album.album_id = track.album_id
 JOIN artist ON artist.artist_id = album.artist_id
 JOIN genre ON genre.genre_id = track.genre_id
 WHERE genre.name LIKE 'Rock'
 GROUP BY artist.artist_id
 ORDER BY number_of_songs DESC
-LIMIT 10;
+LIMIT 10;   
 
 
 /* Q3: Return all the track names that have a song length longer than the average song length. 
@@ -101,6 +104,28 @@ WHERE miliseconds > (
 	SELECT AVG(miliseconds) AS avg_track_length
 	FROM track )
 ORDER BY miliseconds DESC;
+
+
+ /* 2nd method *  ( use a lot of join then query is slow then its take time for execute that reason use optimize query) /
+
+SELECT DISTINCT email AS Email,first_name AS FirstName,last_name AS LastName,genre.name AS Name
+FROM customer
+JOIN invoice ON invoice.customer_id = customer.customer_id
+JOIN invoice_line ON invoice_line.invoice_id = invoice.invoice_id
+JOIN track ON track.track_id = invoice_line.track_id
+JOIN genre ON genre.genre_id = track.genre_id
+WHERE genre.name LIKE 'Rock'
+ORDER BY email;
+
+
+	
+	
+	
+	
+	
+
+
+
 
 
 
@@ -133,7 +158,7 @@ JOIN track t ON t.track_id = il.track_id
 JOIN album alb ON alb.album_id = t.album_id
 JOIN best_selling_artist bsa ON bsa.artist_id = alb.artist_id
 GROUP BY 1,2,3,4
-ORDER BY 5 DESC;
+ORDER BY 5 DESC;     
 
 
 /* Q2: We want to find out the most popular music Genre for each country. We determine the most popular genre as the genre 
